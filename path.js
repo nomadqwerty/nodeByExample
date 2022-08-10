3e6b8ac32fb63eab66c2922f7d5e81e1e947d440
linux <davexmike72@gmail.com>
2022-08-10 23:30:50 +1300

    10.8

---
+++ path.js
@@ -0,0 +1,55 @@
+//path module
+// thjs moule providexs utilities for working with files and directories.
+
+const path = require("path");
+
+// .basename(path,extension)
+const pathPath = path.win32.basename(
+  "../nodeByExample/bufferByExample/buffer.js",
+  "js"
+);
+
+console.log(pathPath);
+
+//.delimiter
+// console.log(process.env.PATH.split(path.delimiter));
+
+// path.dirname:
+console.log(path.dirname("./bufferByExample/buffer.js"));
+
+// .extNme()
+console.log(path.extname("./path.js"));
+
+//.format(pathObj)
+let pathObj = {
+  dir: "c:\\bufferByExample",
+  root: "",
+  base: "",
+  name: "\\buffer",
+  ext: ".js",
+};
+
+console.log(path.format(pathObj));
+
+//.isAbsolute()
+console.log(path.isAbsolute(path.format(pathObj)));
+
+// .join()
+
+const joinPath = path.join(
+  "../../../Desktop/NodeByExample",
+  "../nodeByExample/bufferByExample/buffer.js"
+);
+
+// .normalise
+console.log(path.normalize(joinPath));
+
+// .parse()
+// opposite of path.format
+console.log(path.parse(joinPath));
+
+// path .realative
+const froM = "../../../Desktop/NodeByExample";
+const to = "../nodeByExample/bufferByExample/buffer.js";
+
+console.log(path.relative(froM, to));
