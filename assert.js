343620dff4ddb436f4f1fd446f2d7f6e9a49309b
linux <davexmike72@gmail.com>
2022-08-16 01:46:31 +1300

    15.8

--- assert.js
+++ assert.js
@@ -50,7 +50,7 @@ try {
   assert.deepEqual(obj1, obj2, new Error("it is falsy"));
   assert.deepStrictEqual(obj1, obj2, new Error("it is falsy"));
 } catch (err) {
-  console.log(err.message);
+  // console.log(err.message);
 }
 
 //assert.doesNotMatch(str,reqex,msg)
@@ -59,7 +59,83 @@ try {
   // if there is match an error is thrown because it shouldnnot match
   assert.doesNotMatch("there is a match", /match/, "there is a match");
 } catch ({ message, actual, expected }) {
-  console.log(message);
+  // console.log(message);
 }
 
 //assert.doesNotReject.
+assert.doesNotReject(
+  Promise.reject().catch((err) => console.log(err)),
+  /error/,
+  "message"
+);
+
+assert.doesNotThrow(
+  () => {
+    try {
+      throw new Error("please catch me");
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+  /error/,
+  "message"
+);
+
+// assert.equal(actual, expected,msg)
+// test equality
+
+try {
+  assert.equal(1, true, "i am used for checking if values are ==");
+  // assert.fail(new Error("i am used for throwing assertion errors"));
+  // assert.ifError(new Error("i am used for throw errors to"));
+  // assert.match(
+  //   "this is for strings",
+  //   /strings, my arg is for regex patterns/,
+  //   "this fucntion is used to check if strings are =="
+  // );
+} catch (error) {
+  console.log(error.message, "here");
+}
+
+let obj3 = {
+  a: {
+    b: { c: { [Symbol()]: 1 } },
+  },
+};
+let obj4 = {
+  a: {
+    b: { c: { [Symbol()]: 1 } },
+  },
+};
+
+try {
+  assert.notDeepEqual(obj3, obj3, "i will throw errors if objects are ==");
+} catch (error) {}
+
+// assert.notEqual
+try {
+  assert.notEqual(1, 1, "i throw errors if values are ==");
+} catch (error) {
+  console.log(error.message);
+}
+
+//assert.ok()
+try {
+  assert.ok(0, "i test if values are truthy");
+} catch (error) {
+  console.log(error.message);
+}
+
+try {
+  assert.rejects(
+    () => {
+      return Promise.reject();
+    },
+    {
+      name: "error",
+      message: "rejected promise",
+    }
+  );
+} catch (error) {
+  console.log(error.message);
+}
