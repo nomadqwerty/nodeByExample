dbaba018937881149eee120850e227ced6ecedbd
linux <davexmike72@gmail.com>
2022-08-22 18:36:42 +1300

    19.8

--- console.js
+++ console.js
@@ -4,12 +4,26 @@
 // global console instance configured to the stdout and stderr methods on process.
 // the global console methods arent consistently synchronous like in the web browser. and it is not consistently async like other streams in node
 
-console.error(new Error('error occured'))
-
+try {
+  new Error("error occured");
+} catch (err) {
+  console.log(err);
+}
 // the Console class is used to make simple logger with configurable out put streams.
 // import Console class
-const {Console} = console
+const { Console } = console;
 // console is a global module so require() is not required(lol)
 
-// create new instance of Console.
+// create a write stream, and err stream
+const fs = require("fs");
 
+const writer = fs.createWriteStream("./write.txt", { encoding: "utf8" });
+
+const errWrite = fs.createWriteStream("./write", "utf8");
+// create new instance of Console.
+const logger = new Console(writer, errWrite);
+
+logger.log("from console to stream");
+logger.error(new Error("err message to stream"));
+
+// .clear
