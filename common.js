87532616a286ac59167ddc59c968c88171ed2111
nomadqwerty <davexmike72@gmail.com>
2022-09-08 19:30:49 +0100

    2.9

--- common.js
+++ common.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
+const Fold = require("./Folder");
+// const modules = require("/node_modules");
 // console.log(require.main);
-// console.log(__dirname);
-// console.log(__filename);
+console.log(__dirname);
+console.log(__filename);
 // console.log(process.NODE_PATH);
 // console.log(exports);
 // console.log(require.cache.fs);
 // console.log(require.resolve.paths("./bufferByExample/buffer.js", {}));
-
-console.log(module.id);
-console.log(module.filename);
-console.log(module.isPreloading);
-console.log(module.loaded);
+// console.log(module.id);
+// console.log(module.filename);
+// console.log(module.isPreloading);
+// console.log(module.loaded);
--- ecma.mjs
+++ ecma.mjs
@@ -3,4 +3,22 @@
 // uses import export statements
 
 import common from "./common.js";
-console.log(1);
+import assert from "assert";
+import module from "module";
+
+const es = await import("./es.mjs");
+
+// const require = async (path) => {
+//   return await import(path);
+// };
+// const fs = await require("fs");
+
+//import specifiers are string after from keyword
+const essPath =
+  "file:///C:/Users/davex/OneDrive/Desktop/2ndSemester/Node/nodeByExample/es.mjs";
+console.log(import.meta.url);
+const ess = module.createRequire(essPath);
+
+// console.log(fs.readFileSync("./write.txt").toString());
+//es vs common.js
+console.log(ess);
